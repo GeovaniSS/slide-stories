3b9bc4755d1089e0c23a336cb328ab5069b2d8cd
GeovaniSS <geosilvasousa@gmail.com>
2023-02-18 21:02:48 -0300

    feat: Add useTimeout hook

---
+++ src/hooks/useTimeout.tsx
@@ -0,0 +1,39 @@
+import { useRef, useState } from 'react';
+
+export const useTimeout = () => {
+  const timeoutRef = useRef<number | null>(null);
+  const handlerRef = useRef<TimerHandler>(null!);
+  const [startTime, setStartTime] = useState(0);
+  const [timeLeft, setTimeLeft] = useState(0);
+
+  function timeout(handler: TimerHandler, time: number) {
+    timeoutRef.current = setTimeout(handler, time);
+    handlerRef.current = handler;
+    setStartTime(Date.now());
+    setTimeLeft(time);
+    return timeoutRef.current;
+  }
+
+  function clear() {
+    if (timeoutRef.current) clearTimeout(timeoutRef.current);
+  }
+
+  function pause() {
+    const passedTime = Date.now() - startTime;
+    setTimeLeft((prevTimeLeft) => prevTimeLeft - passedTime);
+    clear();
+  }
+
+  function resume() {
+    clear();
+    timeoutRef.current = setTimeout(handlerRef.current, timeLeft);
+    setStartTime(Date.now());
+  }
+
+  return {
+    timeout,
+    clear,
+    pause,
+    resume,
+  };
+};
